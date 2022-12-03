4eaf633a24ec3c461d6af66dcb7e6472b9bc8e37
KellyC09 <kcarrigan1527@floridapoly.edu>
2022-12-03 00:10:47 -0500

    added a lightning bolt

---
+++ Dungeon/.START~/Delphi~/Door2~/fight.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+Yellow='\033[43m'
+Black='\033[0;33m'
+NC='\033[0m'
+echo -e "
+
+
+                          ${Yellow},/${NC}
+                        ${Yellow},'/${NC}
+                      ${Yellow},' /${NC}
+                    ${Yellow},'  /${NC}
+                  ${Yellow},'   /${NC}
+                ${Yellow},'    /${NC}
+              ${Yellow},'     /${NC}_____,
+            ${Yellow},'           ,'${NC}
+          ${Yellow}.'____       ,'${NC}
+               ${Yellow}/     ,'${NC}
+              ${Yellow}/    ,'${NC}
+             ${Yellow}/   ,'${NC}
+            ${Yellow}/  ,'${NC}
+           ${Yellow}/ ,'${NC}
+          ${Yellow}/,'${NC}
+         ${Yellow}/'${NC}
+${NC}"
